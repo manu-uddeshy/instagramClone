8372e22cfccf88ba1c26ccfc21aae383ff4f407b
manu.uddeshy <uddeshy2@gmail.com>
2019-08-14 10:53:03 +0530

    parse getinbackground, parse findinbackground

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -6,16 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
+
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
-    private Button submitbtn;
+    private Button submitbtn , btngetall;
     private EditText Ename, Pspeed,Ppower,Kspeed,Kpower;
+    private TextView textView1;
+    private String allkickboxers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,45 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         Ppower =(EditText) findViewById(R.id.PunchPowerInput);
         Kspeed = (EditText) findViewById(R.id.KIckspeedinput);
         Kpower= (EditText) findViewById(R.id.kickPowerInput);
+        textView1 = (TextView) findViewById(R.id.Textview1);
+        allkickboxers ="";
+        btngetall = (Button) findViewById(R.id.btngetall);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("3DF9JfNcnU", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object != null && e ==null){
+                            textView1.setText(object.get("name")+"-"+ object.get("punch_power"));
+                        }
+                    }
+                });
+            }
+        });
+        btngetall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+                parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e == null){
+                            if(objects.size()>0){
+                                for(ParseObject KickBoxer : objects){
+                                  allkickboxers = allkickboxers + KickBoxer.get("name")+ "\n";
+                                }
+                                FancyToast.makeText(SignUp.this,allkickboxers,FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                            }
+                        }else {
+                            FancyToast.makeText(SignUp.this,e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
     public void onClick(View v){
         try {
@@ -50,5 +97,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
             FancyToast.makeText(SignUp.this, "Error Occured: "+e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
         }
     }
+
+
 
 }
