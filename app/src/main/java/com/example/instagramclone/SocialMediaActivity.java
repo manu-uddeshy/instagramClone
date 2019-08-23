016d34eb1c55264977a3a8cbf1cc2ebde6cf77a5
manu.uddeshy <uddeshy2@gmail.com>
2019-08-23 09:40:58 +0530

    added frames

--- app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
@@ -1,14 +1,33 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
-
+private ViewPager viewPager;
+private TabAdapter tabAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+//        SocialMediaActivity.this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        viewPager= findViewById(R.id.view_pager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        setupViewPager(viewPager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager){
+TabAdapter adapter = new TabAdapter(getSupportFragmentManager());
+adapter.addFragment(new ProfileTab(), "Profile");
+adapter.addFragment(new UsersTab(), "users");
+adapter.addFragment(new ShareTab(),"share");
+viewPager.setAdapter(adapter);
     }
 }
